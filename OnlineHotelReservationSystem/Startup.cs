20255f297e3ccf894de4847e78f9cbeb353fef46
HuangWei <acer@9599.com>
2016-11-23 14:33:23 +0800

    添加项目文件。

---
+++ OnlineHotelReservationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineHotelReservationSystem.Startup))]
+namespace OnlineHotelReservationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
